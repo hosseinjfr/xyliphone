24d31d93811a0ec376e34fc7ab1de63457f0f334
hossein jafarinejad <hosseinjn@outlook.com>
2021-12-24 23:59:09 +0330

    Add Function to code

--- lib/main.dart
+++ lib/main.dart
@@ -6,29 +6,49 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+
+    AudioCache audioCache = AudioCache();
+
+    void playSound(int soundNumber) async{
+      final file = await audioCache.loadAsFile('note$soundNumber.wav');
+      final bytes = await file.readAsBytes();
+      audioCache.playBytes(bytes);
+    }
+
+    Expanded buildKey({required Color color , required int soundNumber}){
+      return Expanded(
+        child: TextButton(
+          child: const Text(
+              ''
+          ),
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(color),
+          ),
+          onPressed: () {
+            playSound(soundNumber);
+          },
+        ),
+      );
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              child: const Text(
-                'Click Me'
-              ),
-              onPressed: () async {
-                // AudioPlayer audioPlayer = AudioPlayer(mode: PlayerMode.LOW_LATENCY);
-                // await audioPlayer.play('note1.wav' , isLocal: true);
-
-                AudioCache audioCache = AudioCache();
-
-                final file = await audioCache.loadAsFile('note1.wav');
-                final bytes = await file.readAsBytes();
-                audioCache.playBytes(bytes);
-              },
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(color: Colors.red , soundNumber: 1),
+              buildKey(color: Colors.orange , soundNumber: 2),
+              buildKey(color: Colors.green , soundNumber: 3),
+              buildKey(color: Colors.blue , soundNumber: 4),
+              buildKey(color: Colors.yellow , soundNumber: 5),
+              buildKey(color: Colors.blueAccent , soundNumber: 6),
+              buildKey(color: Colors.redAccent , soundNumber: 7),
+            ],
           ),
         ),
       ),
